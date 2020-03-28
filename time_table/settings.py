c2570537adfdc49b90b15e37cc653cf5a3d78bc0
Mikhail Sauchuk <mishasavchuk@gmail.com>
2020-03-28 20:49:28 +0300

    feat: remove env variables

--- time_table/settings.py
+++ time_table/settings.py
@@ -21,10 +21,12 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'owpjiqsq6$39w0mrc)@^46bw!76@%oo2ix)$q7@xou@+oateez'
+# SECRET_KEY = 'owpjiqsq6$39w0mrc)@^46bw!76@%oo2ix)$q7@xou@+oateez'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -127,8 +129,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
